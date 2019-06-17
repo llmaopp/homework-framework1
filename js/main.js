4f789510cdfc055c61343ff9e3937f985b81f6db
llmaopp <duzihe@me.com>
2019-06-17 09:12:04 +0800

    封装marquee

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 $('#marquee').marquee()
+$.fn.anyClick = function() {
+  $(this).on('click', function() {
+    var r = Math.floor(Math.random() * 255)
+    var g = Math.floor(Math.random() * 255)
+    var b = Math.floor(Math.random() * 255)
+    $(this).css('background', `RGB(${r}, ${g}, ${b})`)
+  })
+}
